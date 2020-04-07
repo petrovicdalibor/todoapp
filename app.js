16af18cd3ad8164b977c07c593375f54c45c8902
Dalibor Petrovic <daliborpskopje@gmail.com>
2020-04-07 02:34:23 +0200

    implemented functionality and new structure

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-alert('CONNECTED!');
+// PLUS BUTTON TOGGLES INPUT FOR NEW TODO
+$('#addTodo').on('click', function() {
+  $('#newTodo').fadeToggle();
+});
+
+// ADD NEW TODO FUNCTIONALITY
+$("input[type='text']").on('keydown', function(e) {
+  if(e.which == 13) {
+    // Grabbing new todo text from input
+    let todoText = $(this).val();
+    $(this).val("");
+    // Create a new li and add to ul
+    $('ul').append('<li><span class="delete"><i class="far fa-trash-alt"></i></span> ' + todoText + '</li>');
+  }
+});
+
+// COMPLETED TODO - LINE-THROUGH
+$('ul').on('click', 'li', function() {
+  $(this).toggleClass('completed');
+});
+
+// DELETE TODO
+$('ul').on('click', 'span', function(e) {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  e.stopPropagation();
+});
+
